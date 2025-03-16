939c38d43d82c57286fb7254b48fdeee4641e127
Zoe <zkelepiri@csd.auth.gr>
2025-03-16 12:42:30 +0200

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // ✅ Import this
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
